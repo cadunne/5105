10af36a6233708646be31d009a42b4aa216c1af8
cadunne <dunne064@umn.edu>
2015-02-21 03:39:56 -0600

    Mid trying to get log to work

--- Project_1/src/p1/Client_B.java
+++ Project_1/src/p1/Client_B.java
@@ -11,33 +11,40 @@ import p1.Account;
 
 public class Client_B extends Thread  {
 
+
   protected String host, file;
   protected int port;
+  protected int iterationCount;
   protected int[] aids;
   protected DataInputStream in;
   protected DataOutputStream out;
 
-  Client_B (String host, int port, int[] accounts) {
+  protected Object semaphore = 1;
+
+  Client_B (String host, int port, int iterationCount, int[] accounts) {
     this.host = host;
     this.port = port;
+    this.iterationCount = iterationCount;
     this.aids = accounts;
-    System.out.println ("New client.");
+    // System.out.println ("New client.");
   } 
 
-  private synchronized void writeToLog(String contents){
-    try{
-        FileWriter fw = new FileWriter("clientLogFile.txt", true);
-        fw.write(contents + "\n");
-        fw.flush();
-      }
-      catch(IOException e){
-        e.printStackTrace();
-      }
+  private void writeToLog(String contents){
+    synchronized(this.semaphore){
+        try{
+            FileWriter fw = new FileWriter("clientLogFile.txt", true);
+            fw.write(contents + "\n");
+            fw.close();
+          }
+          catch(IOException e){
+            e.printStackTrace();
+          }
+    }
   }
 
   public void run(){
 
-    System.out.println ("Connecting to " + host + ":" + port + "..");
+    // System.out.println ("Connecting to " + host + ":" + port + "..");
 
     Socket socket = new Socket ( );
     InetSocketAddress addr = new InetSocketAddress(this.host, this.port);//new code
@@ -45,7 +52,7 @@ public class Client_B extends Thread  {
     ObjectInputStream ins = null;
     try{
         socket.connect( addr, this.port ); //addr used to be host
-        System.out.println ("Connected.");
+        // System.out.println ("Connected.");
         OutputStream out = socket.getOutputStream ();
         ous = new ObjectOutputStream( out );
         InputStream in = socket.getInputStream ();
@@ -57,15 +64,17 @@ public class Client_B extends Thread  {
 
 
 
-    for(int i=0; i<10; i++){
+    for(int i=0; i<iterationCount; i++){
         //pic 2 random accounts
         Random generator = new Random(); 
         int j, k, aid_1, aid_2;
         
+        //get two random, not-same ints
         j = generator.nextInt(100);
         do{
             k = generator.nextInt(100);
         }while(k==j);
+
         aid_1 = aids[j]; aid_2 = aids[k];
 
         try{
@@ -99,32 +108,33 @@ public class Client_B extends Thread  {
     Socket      sock = null;
     String host = args[0];
     int  port = Integer.parseInt( args[1] );
+    int  threadCount = Integer.parseInt( args[2] );
+    int  iterationCount = Integer.parseInt( args[3] );
+
 
     //make sure log is made
     FileWriter fw = new FileWriter("clientLogFile.txt", true);
     fw.write("");
     fw.close();
 
-
-    if ( args.length != 2 ) {
-       throw new RuntimeException( "hostname and port number as arguments" );
+    if ( args.length != 4 ) {
+       throw new RuntimeException( "hostname, port, threadCount, and iterationCount as arguments" );
     }
 
-    System.out.println ("Connecting to " + host + ":" + port + "..");
+    // System.out.println ("Connecting to " + host + ":" + port + "..");
 
     Socket socket = new Socket ( );
     InetSocketAddress addr = new InetSocketAddress(host, port);//new code
     socket.connect( addr, port ); //addr used to be host
-    System.out.println ("Connected.");
+    // System.out.println ("Connected.");
 
-
-
-
+    //setup streams
     OutputStream out = socket.getOutputStream ();
     ObjectOutputStream ous = new ObjectOutputStream( out );
     InputStream in = socket.getInputStream ();
     ObjectInputStream ins = new ObjectInputStream (in);
 
+    //local vars
     int[] aids = new int[100];
     Response newRes;
 
@@ -140,6 +150,7 @@ public class Client_B extends Thread  {
         }
     }
 
+    //deposit 100 in all created accounts
     for(int i=0; i<100; i++){
         int accID = aids[i];
         ous.writeObject( new DepositRequest(accID, 100));
@@ -151,7 +162,7 @@ public class Client_B extends Thread  {
         }
     }
 
-    //print total bal
+    //print total balance
     int totalBal = 0;
     for (int i=0; i<100; i++){
         int accID = aids[i];
@@ -165,25 +176,20 @@ public class Client_B extends Thread  {
              e.printStackTrace();
         }
     }
-    System.out.println(totalBal);
+    System.out.println("Total before threads: "+totalBal);
 
 
 
-    Client_B[] threads = new Client_B[100];
+    //create threads and join all of them to this main one.
+    Client_B[] threads = new Client_B[threadCount];
 
-
-    for (int i=0; i<100; i++){
-        Client_B c = new Client_B (host, port, aids);
+    for (int i=0; i<threadCount; i++){
+        Client_B c = new Client_B (host, port, iterationCount, aids);
         c.start ();
         threads[i] = c;
-        // try {
-        //     c.join();
-        // } catch (InterruptedException e) {
-        //     //do nothing
-        // }        threads[i] = c;
     }
 
-    System.out.println("100 Threads All Started");
+    System.out.println("Threads All Started");
 
     for(Thread thread : threads){
         try {
@@ -193,9 +199,9 @@ public class Client_B extends Thread  {
         }
     }
 
-    System.out.println("100 Threads finished");
+    System.out.println("Threads finished");
 
-
+    //check if balance is correct after all threads have finished
     totalBal = 0;
     for (int i=0; i<100; i++){
         int accID = aids[i];
@@ -209,7 +215,7 @@ public class Client_B extends Thread  {
              e.printStackTrace();
         }
     }
-    System.out.println(totalBal);
+    System.out.println("Total after threads: "+totalBal);
 
 
   }

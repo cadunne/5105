63539e50642b294484b9a17d2e638ee76c3afee9
cadunne <dunne064@umn.edu>
2015-02-21 00:52:58 -0600

    Got basic implementation of Client_B working

--- Project_1/src/p1/Client_B.java
+++ Project_1/src/p1/Client_B.java
@@ -3,6 +3,7 @@ package p1;
 import java.net.*;
 import java.io.*;
 import java.lang.Thread;
+import java.util.Random;
 
 import p1.Request;
 import p1.Response;
@@ -12,12 +13,14 @@ public class Client_B extends Thread  {
 
   protected String host, file;
   protected int port;
+  protected int[] aids;
   protected DataInputStream in;
   protected DataOutputStream out;
 
-  Client_B (String host, int port) {
+  Client_B (String host, int port, int[] accounts) {
     this.host = host;
     this.port = port;
+    this.aids = accounts;
     System.out.println ("New client.");
     
     
@@ -26,16 +29,49 @@ public class Client_B extends Thread  {
 
   public void run(){
 
-    // System.out.println ("Connecting to " + host + ":" + port + "..");
+    System.out.println ("Connecting to " + host + ":" + port + "..");
 
-    // Socket socket = new Socket ( );
-    // InetSocketAddress addr = new InetSocketAddress(host, port);//new code
-    // socket.connect( addr, port ); //addr used to be host
-    // System.out.println ("Connected.");
+    Socket socket = new Socket ( );
+    InetSocketAddress addr = new InetSocketAddress(this.host, this.port);//new code
+    ObjectOutputStream ous = null;
+    ObjectInputStream ins = null;
+    try{
+        socket.connect( addr, this.port ); //addr used to be host
+        System.out.println ("Connected.");
+        OutputStream out = socket.getOutputStream ();
+        ous = new ObjectOutputStream( out );
+        InputStream in = socket.getInputStream ();
+        ins = new ObjectInputStream (in);
+    }catch(IOException e){
+        //do nothing
+    }
+
+
+
 
     for(int i=0; i<10; i++){
-        //pick 2 random accounts
-        //transfer 10 between them
+        //pic 2 random accounts
+        Random generator = new Random(); 
+        int j, k, aid_1, aid_2;
+        
+        j = generator.nextInt(100);
+        do{
+            k = generator.nextInt(100);
+        }while(k==j);
+        aid_1 = aids[j]; aid_2 = aids[k];
+
+        try{
+            ous.writeObject( new TransferRequest (aid_1, aid_2, 10));   
+            Response newRes1 = (Response) ins.readObject();
+            String status1 = newRes1.getStatus();
+            System.out.println(status1);
+        }
+        catch ( ClassNotFoundException e) {
+             e.printStackTrace();
+        }
+        catch ( IOException e){
+            e.printStackTrace();
+        }
 
     }
     //terminate
@@ -121,7 +157,7 @@ public class Client_B extends Thread  {
 
 
     for (int i=0; i<100; i++){
-        Client_B c = new Client_B (host, port);
+        Client_B c = new Client_B (host, port, aids);
         c.start ();
         threads[i] = c;
         // try {
@@ -143,6 +179,21 @@ public class Client_B extends Thread  {
 
     System.out.println("100 Threads finished");
 
+
+    totalBal = 0;
+    for (int i=0; i<100; i++){
+        int accID = aids[i];
+        ous.writeObject( new GetBalanceRequest(accID));
+        try {
+            newRes = (Response) ins.readObject();
+            int bal = newRes.getBalance();
+            totalBal += bal;
+        }
+        catch ( ClassNotFoundException e) {
+             e.printStackTrace();
+        }
+    }
+    System.out.println(totalBal);
 
 
   }
